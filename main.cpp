d1984d81b362f45044c25eab1efda8a640a9c5f5
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-11-12 15:40:31 +0100

    chore: roll num and marks input

--- main.cpp
+++ main.cpp
@@ -11,7 +11,16 @@ int main() {
     printf("Welcome to student management system\n");
     printf("Enter your username: ");
     scanf("%s", username);
-    printf("Welcome to student management system: %s", username);
+    printf("Welcome to student management system: %s \n", username);
+
+    printf("Enter your student name: ");
+    scanf("%s", student_name);
+
+    printf("Enter student roll number: ");
+    scanf("%d", &roll_no);
+
+    printf("Enter Student Marks:  ");
+    scanf("%d", &marks);
 
     return 0;
 }
