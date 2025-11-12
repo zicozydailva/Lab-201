785b926020ddfa33ef94131d6af78c0ff3e849dd
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-11-12 15:52:05 +0100

    chore: pass and fail conditionals

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 int main() {
 
+    // DATA STORAGE
     char username[50];
     char student_name[50];
     int roll_no;
     int marks;
 
+    // INPUTS and DATA COLLECTION
     printf("Welcome to student management system\n");
     printf("Enter your username: ");
     scanf("%s", username);
@@ -21,6 +23,13 @@ int main() {
 
     printf("Enter Student Marks:  ");
     scanf("%d", &marks);
+
+    // LOOP TO CHECK IF STUDENT PASSED OR FAILED
+    if (marks > 40) {
+        printf("Student: %s with roll number: %d passed with score of %d", student_name, roll_no, marks);
+    } else {
+        printf("Student: %s with roll number: %d failed with score of %d", student_name, roll_no, marks);
+    }
 
     return 0;
 }
